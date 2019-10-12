41fdd34a5d7759747872bc0ce3bfc9bd73456557
taras-gr <tar.grytsenko@gmail.com>
2019-10-12 20:34:08 +0300

    Added an endpoint for text decrypting

---
+++ InfoSec_Lab2_Backend_API/Models/EncryptedText.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TZI.Models
+{
+    public class EncryptedText
+    {
+        public string CodedText { get; set; }
+    }
+}
---
+++ InfoSec_Lab2_Backend_API/Utils/Ceasar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TZI.Utils
+{
+    public class Caesar
+    {
+        const string alfabet = " .,;-'ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public string Encrypt(string plainMessage)
+        {
+            char theMost = GetLetter(plainMessage);
+            int key = alfabet.IndexOf(theMost);
+            return CodeEncode(plainMessage, key);
+        }
+
+        public string Decrypt(string encryptedMessage)
+        {
+            char theMost = GetLetter(encryptedMessage);
+            int key = alfabet.IndexOf(theMost);
+            return CodeEncode(encryptedMessage, -key);
+        }
+
+        private string CodeEncode(string text, int k)
+        {
+            var letterQty = alfabet.Length;
+            var retVal = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                var c = text[i];
+                var index = alfabet.IndexOf(c);
+                if (index < 0)
+                {
+                    retVal += c.ToString();
+                }
+                else
+                {
+                    var codeIndex = (letterQty + index + k) % letterQty;
+                    retVal += alfabet[codeIndex];
+                }
+            }
+
+            return retVal;
+        }
+
+        private char GetLetter(string text)
+        {
+            return text.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+        }
+    }
+}
