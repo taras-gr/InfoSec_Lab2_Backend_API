678b53a2b2980bde3ce4ad12e969920c89e11e01
taras-gr <tar.grytsenko@gmail.com>
2019-10-19 20:43:02 +0300

    Merged encrypting and decrypting into one controller.

---
+++ InfoSec_Lab2_Backend_API/Controllers/CeasarController.cs
@@ -0,0 +1,43 @@
+﻿using InfoSec_Lab2_Backend_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using TZI.Models;
+using TZI.Utils;
+
+namespace TZI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CeasarController : Controller
+    {
+        // POST api/ceasar/decrypt
+        [HttpPost]
+        [Route("decrypt")]
+        public IActionResult Post([FromBody]EncryptedMessage message)
+        {
+            var alphabet = message.Alphabet;
+            var encryptedText = message.EncryptedText;
+
+            Caesar cipher = new Caesar(alphabet);
+
+            message.EncryptedText = cipher.Decrypt(encryptedText);
+
+            return Json(message);
+        }
+
+        // POST api/ceasar/encrypt
+        [HttpPost]
+        [Route("encrypt")]
+        public IActionResult Post([FromBody]DecryptedMessage message)
+        {
+            var alphabet = message.Alphabet;
+            var key = message.Key;
+            var decryptedText = message.DecryptedText;
+
+            Caesar cipher = new Caesar(alphabet);
+
+            message.DecryptedText = cipher.Encrypt(decryptedText, key);
+
+            return Json(message);
+        }
+    }
+}
