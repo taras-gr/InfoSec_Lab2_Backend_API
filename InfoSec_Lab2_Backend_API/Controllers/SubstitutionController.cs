7e5aac7617d55c3c09257e9a8df4fd647ccd4aca
Taras.Hrytsenko <Taras.Hrytsenko@thomsonreuters.com>
2019-10-22 16:54:03 +0300

    Added Substitution cipher.

---
+++ InfoSec_Lab2_Backend_API/Controllers/SubstitutionController.cs
@@ -0,0 +1,43 @@
+﻿using InfoSec_Lab2_Backend_API.Models;
+using InfoSec_Lab2_Backend_API.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfoSec_Lab2_Backend_API.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class SubstitutionController : Controller
+    {
+        // POST api/substitution/decrypt
+        [HttpPost]
+        [Route("decrypt")]
+        public IActionResult DecryptMessage([FromBody]VigenereModel message)
+        {
+            var encryptedText = message.Message;
+            var alphabet = message.Alphabet;
+            var password = message.Password;
+
+            VigenereCipher cipher = new VigenereCipher(alphabet);
+
+            message.Message = cipher.Decrypt(encryptedText, password);
+
+            return Json(message);
+        }
+
+        // POST api/substitution/encrypt
+        [HttpPost]
+        [Route("encrypt")]
+        public IActionResult EncryptMessage([FromBody]VigenereModel message)
+        {
+            var plainText = message.Message;
+            var alphabet = message.Alphabet;
+            var password = message.Password;
+
+            Substitution cipher = new Substitution(alphabet);
+
+            message.Message = cipher.Encrypt(plainText, password);
+
+            return Json(message);
+        }
+    }
+}
---
+++ InfoSec_Lab2_Backend_API/Utils/Substitution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace InfoSec_Lab2_Backend_API.Utils
+{
+    public class Substitution
+    {
+        private readonly string _alphabet;
+
+        public Substitution(string alphabet)
+        {
+            _alphabet = alphabet;
+        }
+
+        public string Encrypt(string text, string key)
+        {
+            var substitutionAlhabet = key.ToCharArray();
+            char[] message = text.ToCharArray();
+
+            string encryptedMessage = "";
+            
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                encryptedMessage += substitutionAlhabet[_alphabet.IndexOf(message[i])].ToString();
+            }
+
+            return encryptedMessage;
+        }
+    }
+}
